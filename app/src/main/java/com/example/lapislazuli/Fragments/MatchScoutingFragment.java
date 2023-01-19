f1221e507217cf1a44383e6dc560fdd8ee103564
fatima <fatimaa@vandenrobotics.com>
2023-01-18 16:25:32 -0800

    buttons were coded
    match scouting button go to match scouting start and vice versa

--- app/src/main/java/com/example/lapislazuli/Fragments/MatchScoutingFragment.java
+++ app/src/main/java/com/example/lapislazuli/Fragments/MatchScoutingFragment.java
@@ -1,4 +1,44 @@
 package com.example.lapislazuli.Fragments;
 
-public class MatchScoutingFragment {
+import static com.example.lapislazuli.databinding.FragmentMatchScoutingBinding.inflate;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.lapislazuli.R;
+import com.example.lapislazuli.databinding.FragmentMatchScoutingBinding;
+
+public class MatchScoutingFragment extends Fragment {
+
+    private FragmentMatchScoutingBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+
+        binding = FragmentMatchScoutingBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        Button submit = root.findViewById(R.id.bt_submitMatch);
+
+        submit.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(),R.id.nav_host_fragment_content_main);
+                navController.navigate(R.id.nav_match_scouting_start);
+            }
+        });
+
+        return root;
+    }
+
+
 }
